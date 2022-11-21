99f7cefe13eda83bd00a8c95ed2310a1c2c0e8d8
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-22 00:09:17 +0530

    Update after gnome-extensions review

---
+++ extension.js
@@ -0,0 +1,290 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const {
+    GObject, St, GLib, Clutter, Gio,
+} = imports.gi;
+
+const Main = imports.ui.main;
+const Target = Main.panel.statusArea.quickSettings._system._systemItem.child;
+const {QuickSettingsItem} = imports.ui.quickSettings;
+const SystemActions = imports.misc.systemActions;
+const BindFlags = GObject.BindingFlags.DEFAULT | GObject.BindingFlags.SYNC_CREATE;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const iconsPath = Me.dir.get_child('icons').get_path();
+
+const SUSPEND = 'suspend';
+const HYBRID_SLEEP = 'hybrid_sleep';
+const HIBERNATE = 'hibernate';
+const SWITCH_USER = 'switch_user';
+const LOGOUT = 'logout';
+const RESTART = 'restart';
+const POWEROFF = 'poweroff';
+
+/**
+ *
+ * @param {string} input - HybridSleep/Hibernate
+ */
+function hybridSleepOrHibernate(input) {
+    const LoginManager = imports.misc.loginManager.getLoginManager();
+    if (LoginManager._proxy)
+        LoginManager._proxy.call(input, GLib.Variant.new('(b)', [true]), Gio.DBusCallFlags.NONE, -1, null, null);
+}
+
+let items;
+let keys;
+
+const LabelLauncher = new GObject.registerClass(
+    class LabelLauncher extends St.Widget {
+        _init() {
+            this.label = new St.Label({style_class: 'dash-label'});
+            this.label.hide();
+            Main.layoutManager.addTopChrome(this.label);
+        }
+
+        showLabel(button) {
+            this.label.set_text(button.accessible_name);
+            this.label.opacity = 0;
+            this.label.show();
+
+            const center = button.get_transformed_position();
+            const x = center[0] - 20;
+            const y = 10;
+
+            this.label.set_position(x, y);
+            this.label.ease({
+                opacity: 255,
+                duration: 100,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+            });
+        }
+
+        hideLabel() {
+            this.label.ease({
+                opacity: 0,
+                duration: 500,
+                mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+                onComplete: () => this.label.hide(),
+            });
+        }
+    }
+);
+
+const SyncLabel = GObject.registerClass(
+    class SyncLabel extends QuickSettingsItem {
+        _init(item) {
+            this.item = item;
+            this.item._showLabelTimeoutId = 0;
+            this.item._resetHoverTimeoutId = 0;
+            this.item._labelShowing = false;
+            this.toolTip = new LabelLauncher();
+            this.toolTip.child = this.item;
+        }
+
+        _syncLabel() {
+            if (this.toolTip.child.hover) {
+                if (this.item._showLabelTimeoutId === 0) {
+                    const timeout = this.item._labelShowing ? 0 : 100;
+                    this.item._showLabelTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, timeout,
+                        () => {
+                            this.item._labelShowing = true;
+                            this.toolTip.showLabel(this.item);
+                            this.item._showLabelTimeoutId = 0;
+                            return GLib.SOURCE_REMOVE;
+                        });
+                    GLib.Source.set_name_by_id(this._showLabelTimeoutId, '[gnome-shell this.toolTip.showLabel');
+                    if (this.item._resetHoverTimeoutId > 0) {
+                        GLib.source_remove(this.item._resetHoverTimeoutId);
+                        this.item._resetHoverTimeoutId = 0;
+                    }
+                }
+            } else {
+                if (this.item._showLabelTimeoutId > 0)
+                    GLib.source_remove(this.item._showLabelTimeoutId);
+                this.item._showLabelTimeoutId = 0;
+                this.toolTip.hideLabel();
+                if (this.item._labelShowing) {
+                    this.item._resetHoverTimeoutId = GLib.timeout_add(
+                        GLib.PRIORITY_DEFAULT, 100,
+                        () => {
+                            this.item._labelShowing = false;
+                            this.item._resetHoverTimeoutId = 0;
+                            return GLib.SOURCE_REMOVE;
+                        }
+                    );
+                    GLib.Source.set_name_by_id(this.item._resetHoverTimeoutId, '[gnome-shell] this.item._labelShowing');
+                }
+            }
+        }
+    });
+
+const CreateItem = GObject.registerClass(
+    class CreateItem extends QuickSettingsItem {
+        _init(ICON_NAME, ACCESSIBLE_NAME, ACTION, KEY = null, BINDING_ID = null) {
+            super._init({
+                style_class: 'icon-button',
+                can_focus: true,
+                track_hover: true,
+                child: ICON_NAME.startsWith('bosm-') ? new St.Icon({gicon: Gio.icon_new_for_string(`${iconsPath}/${ICON_NAME}`)}) : new St.Icon({icon_name: ICON_NAME}),
+                accessible_name: ACCESSIBLE_NAME,
+            });
+
+            const TakeAction = new SystemActions.getDefault();
+
+            this.connect('clicked', () => {
+                switch (ACTION) {
+                case SUSPEND:
+                    TakeAction.activateSuspend();
+                    break;
+                case HYBRID_SLEEP:
+                    hybridSleepOrHibernate('HybridSleep');
+                    break;
+                case HIBERNATE:
+                    hybridSleepOrHibernate('Hibernate');
+                    break;
+                case SWITCH_USER:
+                    TakeAction.activateSwitchUser();
+                    break;
+                case LOGOUT:
+                    TakeAction.activateLogout();
+                    break;
+                case RESTART:
+                    TakeAction.activateRestart();
+                    break;
+                case POWEROFF:
+                    TakeAction.activatePowerOff();
+                    break;
+                }
+
+                Main.panel.closeQuickSettings();
+            });
+
+            if (BINDING_ID)
+                TakeAction.bind_property(BINDING_ID, this, 'visible', BindFlags);
+
+            const callSync = new SyncLabel(this);
+            this.connect('notify::hover', () => {
+                callSync._syncLabel();
+            });
+
+            items.push(this);
+            keys.push(KEY);
+        }
+    }
+);
+
+let removed = Target.get_children()[6];
+
+const BringOutExtension = new GObject.registerClass(
+    class BringOutExtension extends QuickSettingsItem {
+        _init() {
+            Target.remove_child(removed);
+            this._settings = ExtensionUtils.getSettings();
+            this._createMenu();
+            this._connectSettings();
+        }
+
+        _createMenu() {
+            items = [];
+            keys = [];
+            this._suspendItem = new CreateItem('media-playback-pause-symbolic', 'Suspend', SUSPEND, 'remove-suspend-button', 'can-suspend');
+            this._hybridSleepItem = new CreateItem('bosm-hybrid-sleep-symbolic.svg', 'Hybrid Sleep', HYBRID_SLEEP, 'remove-hybrid-sleep-button');
+            this._hibernateItem = new CreateItem('bosm-hibernate-symbolic.svg', 'Hibernate', HIBERNATE, 'remove-hibernate-button');
+            this._switchUserItem = new CreateItem('system-switch-user-symbolic', 'Switch User…', SWITCH_USER, null, 'can-switch-user');
+            this._logoutItem = new CreateItem('system-log-out-symbolic', 'Log Out…', LOGOUT, 'remove-logout-button', 'can-logout');
+            this._restartItem = new CreateItem('system-reboot-symbolic', 'Restart…', RESTART, 'remove-restart-button', 'can-restart');
+            this._powerItem = new CreateItem('system-shutdown-symbolic', 'Power Off…', POWEROFF, 'remove-power-button', 'can-power-off');
+
+            items.forEach((item, index) => {
+                let boolean;
+                let key = keys[index];
+                if (key) {
+                    boolean = this._settings.get_boolean(key);
+                    if (!boolean)
+                        Target.add(item);
+                } else {
+                    Target.add(item);
+                }
+            });
+        }
+
+        _connectSettings() {
+            items.forEach((item, index) => {
+                let key = keys[index];
+                if (key)
+                    item._buttonShowHide = this._settings.connect(`changed::${key}`, this._settingsChanged.bind(this));
+            });
+        }
+
+        _settingsChanged() {
+            items.forEach(item => {
+                if (item)
+                    Target.remove_child(item);
+            });
+            this._createMenu();
+        }
+
+        _destroy() {
+            items.forEach(item => {
+                if (item._resetHoverTimeoutId) {
+                    GLib.source_remove(item._resetHoverTimeoutId);
+                    item._resetHoverTimeoutId = null;
+                }
+
+                if (item._showLabelTimeoutId) {
+                    GLib.source_remove(item._showLabelTimeoutId);
+                    item._showLabelTimeoutId = null;
+                }
+
+                if (item._buttonShowHide)
+                    this._settings.disconnect(item._buttonShowHide);
+
+                item.destroy();
+                item = null;
+            });
+
+            Target.add_child(removed);
+            items = [];
+            keys = [];
+        }
+    }
+);
+
+let modifiedMenu;
+
+class Extension {
+    enable() {
+        modifiedMenu = new BringOutExtension();
+    }
+
+    disable() {
+        modifiedMenu._destroy();
+        modifiedMenu.destroy();
+        modifiedMenu = null;
+    }
+}
+
+/**
+ *
+ */
+function init() {
+    return new Extension();
+}
---
+++ prefs.js
@@ -0,0 +1,114 @@
+const {Gtk} = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+/**
+ *
+ */
+function init() {
+}
+
+/**
+ *
+ */
+function buildPrefsWidget() {
+    let widget = new PrefsWidget();
+    return widget.widget;
+}
+
+class CreateButton {
+    constructor(label, key) {
+        this._label = label;
+        this._key = key;
+    }
+
+    _createButton() {
+        this._settings = ExtensionUtils.getSettings();
+        const hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_top: 5});
+        const buttonLabel = new Gtk.Label({label: this._label, xalign: 0, hexpand: true});
+        const toggleSwitch = new Gtk.Switch({active: this._settings.get_boolean(this._key)});
+        toggleSwitch.connect('notify::active', button => {
+            this._settings.set_boolean(this._key, button.active);
+        });
+
+        hbox.append(buttonLabel);
+        hbox.append(toggleSwitch);
+
+        return hbox;
+    }
+}
+
+const createGrid = () =>
+    new Gtk.Grid({
+        column_spacing: 12, row_spacing: 12,
+        column_homogeneous: true,
+        hexpand: true, vexpand: true,
+        margin_start: 14, margin_end: 14, margin_top: 14, margin_bottom: 14,
+        visible: true,
+    });
+
+
+const createVBox = () =>
+    new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, spacing: 10, visible: true});
+
+
+class PrefsWidget {
+    constructor() {
+        this.widget = new Gtk.Grid({visible: true, column_homogeneous: true});
+        this.notebook = new Gtk.Notebook({visible: true});
+
+        this.widget.attach(this.notebook, 0, 0, 1, 1);
+
+        // Basic Settings Page
+
+        const basicPageGrid = createGrid();
+        const basicPageVBox = createVBox();
+
+        basicPageGrid.attach(basicPageVBox, 0, 0, 3, 1);
+
+        this._suspendButton = new CreateButton('Remove Suspend Button', 'remove-suspend-button');
+        basicPageVBox.append(this._suspendButton._createButton());
+
+        this._logoutButton = new CreateButton('Remove Logout Button', 'remove-logout-button');
+        basicPageVBox.append(this._logoutButton._createButton());
+
+        this._restartButton = new CreateButton('Remove Restart Button', 'remove-restart-button');
+        basicPageVBox.append(this._restartButton._createButton());
+
+        this._powerButton = new CreateButton('Remove Poweroff Button', 'remove-power-button');
+        basicPageVBox.append(this._powerButton._createButton());
+
+        this.notebook.append_page(basicPageGrid, new Gtk.Label({label: 'Basic Settings', visible: true, hexpand: true}));
+
+        // End of Basic Settings Page
+
+        // HybridSleep/Hibernate Page
+
+        const HibernatePageGrid = createGrid();
+        const HibernatePageVBox = createVBox();
+
+        HibernatePageGrid.attach(HibernatePageVBox, 0, 0, 3, 1);
+
+        this._hybridSleepButton = new CreateButton('Remove HybridSleep Button', 'remove-hybrid-sleep-button');
+        HibernatePageVBox.append(this._hybridSleepButton._createButton());
+
+        this._hibernationButton = new CreateButton('Remove Hibernation Button', 'remove-hibernate-button');
+        HibernatePageVBox.append(this._hibernationButton._createButton());
+
+        const labelText = '<span size="medium">' +
+            'This page is useful only if you have configured HybridSleep and Hibernation yourself,\n' +
+            'some useful links on this topic are ' +
+            '<a href="https://ubuntuhandbook.org/index.php/2021/08/enable-hibernate-ubuntu-21-10/">Link 1</a> ' +
+            '<a href="https://github.com/arelange/gnome-shell-extension-hibernate-status#hibernation-button-does-not-show-up-but-systemctl-hibernate-works">Link 2</a> ' +
+            '<a href="https://support.system76.com/articles/enable-hibernation/">Link 3</a> ' +
+            '<a href="https://extensions.gnome.org/extension/755/hibernate-status-button/">Source</a> ' +
+            '<a href="https://github.com/PRATAP-KUMAR/bring-out-submenu-of-power-off-logout/issues/28">Github Issue</a>' +
+            '</span>';
+
+        let warningLabel = new Gtk.Label({label: labelText, xalign: 0, hexpand: true, use_markup: true});
+        HibernatePageVBox.append(warningLabel);
+
+        this.notebook.append_page(HibernatePageGrid, new Gtk.Label({label: 'HybridSleep/Hibernate', visible: true, hexpand: true}));
+
+        // End of HybridSleep/Hibernate Page
+    }
+}
