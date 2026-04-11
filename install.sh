a404e784979941bb570a7272e87c3440f5d46cb2
PRATAP PANABAKA <pratap.panabaka@gmail.com>
2026-04-11 14:21:46 +0530

    add support for GNOME 50

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo -e "\trunning the script...\n"
 echo -e "\t1. gnome-shell version $SHELL_VERSION detected"
 
 if [[ $SHELL_VERSION -ge 45 ]];then
-  cd src/v-45-46-47-48-49
+  cd src/v-45-46-47-48-49-50
 fi
 
 echo -e "\t2. Creating zip file..."
